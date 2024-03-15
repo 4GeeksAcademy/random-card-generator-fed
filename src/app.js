630758a1a5555a92580f240d6e43f7df42b5a0af
Federico Moret <106476837+shlimsady@users.noreply.github.com>
2024-03-15 17:38:29 +0000

    changes made

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function generateRandomCard() {
+  const suits = ["heart", "spade", "club", "diamond"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  return { suit: randomSuit, value: randomValue };
+}
+
+window.onload = () => {
+  const { suit, value } = generateRandomCard();
+  const card = document.querySelector(".card");
+
+  card.classList.add(suit);
+  card.querySelector(".number").textContent = value;
 };
